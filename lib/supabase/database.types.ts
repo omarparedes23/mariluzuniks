de517669e670f4b1aea302c52ca14675d19ddc1a
Omar Paredes <omarparedes23@gmail.com>
2026-05-28 18:06:54 +0200

    fix costos

--- lib/supabase/database.types.ts
+++ lib/supabase/database.types.ts
@@ -14,6 +14,7 @@ export interface Database {
           id: string
           nombre: string
           stock: number
+          precio_costo: number | null
           precio: number
           imagen_url_r2: string | null
           created_at: string
@@ -23,6 +24,7 @@ export interface Database {
           id?: string
           nombre: string
           stock?: number
+          precio_costo?: number | null
           precio: number
           imagen_url_r2?: string | null
           created_at?: string
@@ -32,6 +34,7 @@ export interface Database {
           id?: string
           nombre?: string
           stock?: number
+          precio_costo?: number | null
           precio?: number
           imagen_url_r2?: string | null
           created_at?: string
